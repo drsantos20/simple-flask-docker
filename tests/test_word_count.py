6f8c538f13ad6c089c899798dd75d280c3a09014
Daniel Ribeiro dos Santos <daniel.rsantos@b2wdigital.com>
2018-08-01 15:33:10 -0300

    add some unit tests

--- tests/test_word_count.py
+++ tests/test_word_count.py
@@ -1,24 +1,33 @@
 import unittest
 import sys
 sys.path.append('../')
-import count
+import main
 import requests
 import json
 import sys
 
+###Integration Test
 class TestFlaskApiUsingRequests(unittest.TestCase):
     def setUp(self):
-        self.app = count.app.test_client()
+        self.app = main.app.test_client()
 
     def test_hello_world(self):
         response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
 
+    def test_hello_world_error(self):
+        response = self.app.post('/')
+        self.assertEqual(response.status_code, 400)
 
+###Unit Test
 class TestFlaskApi(unittest.TestCase):    
     def test_word_count(self):
-        result = count.wordcount("one two three four five")
+        result = main.wordcount("one two three four five")
         self.assertEqual(result, 5)
+
+    def test_word_count_empty(self):
+        result = main.wordcount("")
+        self.assertEqual(result, 0)
 
 
 if __name__ == "__main__":
